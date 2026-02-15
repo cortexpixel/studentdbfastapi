bcea2883d86457a32bb81a139881e8a4719c0222
cortexpixel <cortexpixel@gmail.com>
2026-02-15 20:17:44 +0530

    added git ignore, db updated

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
